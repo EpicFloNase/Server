009e1215c5e358cd874ea7a1889f7f50d8e5d3e5
Derpo <epicflonase@gmail.com>
2019-04-28 16:16:22 -0400

    Added /boi for testing

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/boi', (req, res) => {
+    res.send('boi');
+});
+
+// custom 404 error page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Server started in http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
